c860ebf9db0d1fd83a27115f61ab67573a605d95
Enes Golic <enesgolic@live.dk>
2020-09-17 12:45:41 +0200

    Added error handling and Swagger documentation

---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,32 @@
+
+using System;
+
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+        }
+
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; }
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "A bad request",
+                401 => "Not authorized",
+                404 => "Resource not found",
+                500 => "Server Error",
+                _ => null
+            };
+        }
+
+    }
+
+}
